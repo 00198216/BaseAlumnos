25d32e6d4c65d19857be71783e063346ba3fde43
Sebastian <charlzbach14@gmail.com>
2018-05-20 01:57:56 -0600

    Cardview added

---
+++ app/src/main/java/com/example/charl/basealumnos/Cardview/cardview.java
@@ -0,0 +1,15 @@
+package com.example.charl.basealumnos.Cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.charl.basealumnos.R;
+
+public class cardview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+    }
+}
